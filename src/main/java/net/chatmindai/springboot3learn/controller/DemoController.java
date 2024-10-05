88cf7e84501a6603247970471d7c3a87ce5358a8
张坤 <2553150278@qq.com>
2024-10-05 22:55:41 +0800

    feat: 3. 基于AOP的开发

--- src/main/java/net/chatmindai/springboot3learn/controller/DemoController.java
+++ src/main/java/net/chatmindai/springboot3learn/controller/DemoController.java
@@ -3,18 +3,11 @@ package net.chatmindai.springboot3learn.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import jakarta.validation.Valid;
+import net.chatmindai.springboot3learn.annotation.LogInfo;
 import net.chatmindai.springboot3learn.entity.demo.dto.DemoDTO;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -63,10 +56,10 @@ public class DemoController {
 
     // @DeleteMapping("/delete/{id}")
     // public ResponseEntity<?> deleteSomething(@PathVariable Long id) { ... }
+    @LogInfo("获取用户信息")
     @Operation(summary = "使用DemoDTO对象")
     @PostMapping("/demo2")
     public DemoDTO useDemoDTO(@Validated @RequestBody DemoDTO demoDTO) {
-        log.info("入参为: {}", demoDTO);
         return demoDTO;
     }
 }
