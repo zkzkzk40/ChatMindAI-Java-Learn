dd2f116dd061925ad54680e0f2a2e1c1e8e79493
张坤 <2553150278@qq.com>
2024-10-11 10:37:49 +0800

    feat: 5.6 补充一个Mybatis使用样例

--- src/main/java/net/chatmindai/springboot3learn/mapper/UserMapper.java
+++ src/main/java/net/chatmindai/springboot3learn/mapper/UserMapper.java
@@ -1,16 +1,14 @@
 package net.chatmindai.springboot3learn.mapper;
 
-import net.chatmindai.springboot3learn.entity.user.User;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import net.chatmindai.springboot3learn.entity.user.User;
+import net.chatmindai.springboot3learn.entity.user.dtos.UserQueryDto;
 
-/**
-* @author zhangkun
-* @description 针对表【demo_user(演示用户信息表)】的数据库操作Mapper
-* @createDate 2024-10-08 14:41:15
-* @Entity net.chatmindai.springboot3learn.entity.user.User
-*/
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface UserMapper extends BaseMapper<User> {
-
+    User selectUserById(UserQueryDto dto);
 }
 
 
