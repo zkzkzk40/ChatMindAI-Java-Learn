5eab98917a41901e37ea981104d097ddc0cc5b4a
11375 <11375@RED_FOCALORS>
2024-10-23 19:36:16 +0800

    hello?

---
+++ src/main/java/com/example/splearn/SpLearnApplication.java
@@ -0,0 +1,14 @@
+package com.example.splearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpLearnApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/example/splearn/SpLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.splearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
