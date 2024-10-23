441d83570ad75d74e6e7ec387c29cd1368fa4268
11375 <11375@RED_FOCALORS>
2024-10-23 23:33:43 +0800

    接收校验入参和Apifox的使用(对目录格式稍加优化)

--- src/main/java/net/chatmindai/splearn/SpLearnApplication.java
+++ src/main/java/net/chatmindai/splearn/SpLearnApplication.java
@@ -1,4 +1,4 @@
-package com.example.splearn;
+package net.chatmindai.splearn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/net/chatmindai/splearn/controller/DemoController.java
@@ -0,0 +1,57 @@
+package net.chatmindai.splearn.controller;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import net.chatmindai.splearn.entity.demo.dto.DemoDTO;
+/**
+ * Controller demo
+ * 这是跟着学习的示范类
+ *
+ * @author RedFocalors
+ * @date 2024/10/23
+ */
+@RestController // 表示这是一个RESTful Web服务的控制器,组合了@Controller和@ResponseBody
+@RequestMapping("/demo") // 定义该控制器的基础URL路径
+@Slf4j
+public class DemoController {
+/**
+ * 处理GET请求的方法
+ * @GetMapping是@RequestMapping(method = RequestMethod.GET)的简写
+ * 其他HTTP方法还有:
+ * @PostMapping - 处理POST请求
+ * @PutMapping - 处理PUT请求
+ * @DeleteMapping - 处理DELETE请求
+ * @PatchMapping - 处理PATCH请求
+ */
+@GetMapping("/hello")
+public Object hello(){
+    // 创建一个Map对象并初始化,使用Java 9引入的Map.of()方法
+    Map<String,Object> map = new java.util.HashMap<>(Map.of("name", "chatmindai", "age", 18));
+
+    // 向map中添加新的键值对,key&value
+    map.put("introduction","We are ChatMindAI");
+
+    // 直接返回map对象,Spring会自动将其转换为JSON格式
+    // 如果想要更细粒度的控制,可以考虑使用ResponseEntity<>
+    return map;
+}
+
+
+@PostMapping("/demo2")
+public DemoDTO useDemoDTO(@Validated @RequestBody DemoDTO demoDTO) {
+    log.info("入参为: {}", demoDTO);
+    return demoDTO;
+}
+    // 可以添加更多的方法来处理不同的请求
+    // 例如:
+    // @PostMapping("/create")
+    // public ResponseEntity<?> createSomething(@RequestBody SomeDTO dto) { ... }
+
+    // @PutMapping("/update/{id}")
+    // public ResponseEntity<?> updateSomething(@PathVariable Long id, @RequestBody SomeDTO dto) { ... }
+
+    // @DeleteMapping("/delete/{id}")
+    // public ResponseEntity<?> deleteSomething(@PathVariable Long id) { ... }
+}
