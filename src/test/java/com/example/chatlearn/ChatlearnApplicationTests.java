8cdf5b2c5b8c4082afa8742485b2787e86bad5cf
吕成刚 <1330728137@qq.com>
2024-10-26 00:20:54 +0800

    hhh

---
+++ src/test/java/com/example/chatlearn/ChatlearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatlearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
