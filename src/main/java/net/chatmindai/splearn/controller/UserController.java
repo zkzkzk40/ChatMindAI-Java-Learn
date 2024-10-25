444b331deb4027db6c3dcd8ba761f757cd647c6c
11375 <11375@RED_FOCALORS>
2024-10-25 15:46:09 +0800

    完成了修改的功能,现完成功能:增,改,查

--- src/main/java/net/chatmindai/splearn/controller/UserController.java
+++ src/main/java/net/chatmindai/splearn/controller/UserController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.*;
 
 import jakarta.validation.Valid;
 
+import java.time.LocalDateTime;
+
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
@@ -55,5 +57,43 @@ public class UserController {
             return CommonResult.error("未查找到该用户");
         }
         return CommonResult.success(foundUser, "成功找到该用户");
+    }
+
+    @PutMapping("/edit")
+    @Operation(summary = "修改用户信息",description = "先查询用户是否存在,再接收修改信息,但不能违规")
+    public CommonResult<User> updateUser(@RequestParam String id,@Valid @RequestBody DemoDTO demoDTO) {
+        UserQueryDto dto = new UserQueryDto();
+        if (id == null || id.trim().isEmpty()) {
+            return CommonResult.error("id为空");
+        }
+        Long userId;
+        try {
+            userId = Long.valueOf(id);
+        } catch (Exception e) {
+            return CommonResult.error("id格式错误");
+        }
+        dto.setId(userId);
+        User foundUser = userMapper.selectUserById(dto);
+        if (foundUser == null) {
+            return CommonResult.error("该用户不存在");
+        }
+        User dataUser = UserConverter.INSTANCE.userDtoToUser(demoDTO);
+        LocalDateTime now = LocalDateTime.now();
+        User user = userService.getById(userId);
+        user.setName(dataUser.getName());
+        user.setAge(dataUser.getAge());
+        user.setEmail(dataUser.getEmail());
+        user.setPhoneNumber(dataUser.getPhoneNumber());
+        user.setBirthDate(dataUser.getBirthDate());
+        user.setPlanDate(dataUser.getPlanDate());
+        user.setScore(dataUser.getScore());
+        user.setHobbies(dataUser.getHobbies());
+        user.setUpdateAt(now);
+        boolean update = userService.updateById(user);
+        if (update) {
+            return CommonResult.success(user, "用户更新成功");
+        } else {
+            return CommonResult.error("用户更新失败");
+        }
     }
 }
