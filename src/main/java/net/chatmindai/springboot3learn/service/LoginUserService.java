e94a9b2dfec1bccec715175488cc13d35617767f
bug-py666 <930333105@qq.com>
2024-10-27 02:51:36 +0800

    feat: 项目初始化

---
+++ src/main/java/net/chatmindai/springboot3learn/service/LoginUserService.java
@@ -0,0 +1,7 @@
+package net.chatmindai.springboot3learn.service;
+
+import net.chatmindai.springboot3learn.entity.user.loginUser;
+
+public interface LoginUserService {
+    loginUser login(loginUser loginuser);
+}
