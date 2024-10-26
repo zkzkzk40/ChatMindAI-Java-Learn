35b03f16ad1c4d681f31a0cabbd00e4e30ab9efa
bug-py666 <930333105@qq.com>
2024-10-26 18:31:15 +0800

    feat: 项目初始化

--- src/main/java/net/chatmindai/springboot3learn/AOP/AOP.java
+++ src/main/java/net/chatmindai/springboot3learn/AOP/AOP.java
@@ -1,21 +1,34 @@
 package net.chatmindai.springboot3learn.AOP;
 
-import net.chatmindai.springboot3learn.annotation.LogInfo;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
+@Aspect
+@Component
 public class AOP {
-    @Around("controllerPointcut()")
-    public Object logAroundController(ProceedingJoinPoint joinPoint) throws Throwable {
+
+    private static final Logger log = LoggerFactory.getLogger(AOP.class);
+
+    @Pointcut("execution(* net.chatmindai.springboot3learn.controller..*.*(..))")
+    public void controllerPointcut() {
+    }
+
+@Around("controllerPointcut()")
+public Object logAroundController(ProceedingJoinPoint joinPoint) throws Throwable {
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
         LogInfo logInfo = signature.getMethod().getAnnotation(LogInfo.class);
 
         // 如果方法没有 @LogInfo 注解，直接执行方法并返回结果
         if (logInfo == null) {
-            return joinPoint.proceed();
+        return joinPoint.proceed();
         }
 
         String methodDescription = logInfo.value();
@@ -32,5 +45,4 @@ public class AOP {
         log.info("出参: {}", result);
 
         return result;
-    }
-}
+        }
