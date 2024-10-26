a8db63d1caa6bfda67f9ec6f3533da226f22424c
bug-py666 <930333105@qq.com>
2024-10-26 21:12:39 +0800

    feat: 项目初始化

--- src/main/java/net/chatmindai/springboot3learn/exception/GlobalExceptionHandler.java
+++ src/main/java/net/chatmindai/springboot3learn/exception/GlobalExceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.method.annotation.MethodArgumentNotValidException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import java.util.HashMap;
 import java.util.Map;
