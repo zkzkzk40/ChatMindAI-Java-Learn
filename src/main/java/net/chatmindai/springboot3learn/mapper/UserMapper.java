96d4b50d8ed224020d9443df15ed2f8cd6618e30
bug-py666 <930333105@qq.com>
2024-10-27 01:06:22 +0800

    feat: 项目初始化

--- src/main/java/net/chatmindai/springboot3learn/mapper/UserMapper.java
+++ src/main/java/net/chatmindai/springboot3learn/mapper/UserMapper.java
@@ -1,18 +1,12 @@
 package net.chatmindai.springboot3learn.mapper;
 
-import net.chatmindai.springboot3learn.entity.user.User;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import net.chatmindai.springboot3learn.entity.user.User;
+import net.chatmindai.springboot3learn.entity.user.dtos.UserQueryDto;
 
-/**
-* @author 瓜瓜乐
-* @description 针对表【demo_user(演示用户信息表)】的数据库操作Mapper
-* @createDate 2024-10-26 21:48:50
-* @Entity net.chatmindai.springboot3learn.entity.user.DemoUser
-*/
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface UserMapper extends BaseMapper<User> {
-
+    User selectUserById(UserQueryDto dto);
 }
-
-
-
-
