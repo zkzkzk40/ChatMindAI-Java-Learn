dec0bcf9187b906228dc5ac1504900148c792404
yaoayao1 <2399482262@qq.com>
2024-11-01 14:14:36 +0800

    feat: 补充一个mybatis使用样例

---
+++ src/main/java/net/chatmindai/springboot3learn/entity/user/dtos/UserQueryDto.java
@@ -0,0 +1,13 @@
+package net.chatmindai.springboot3learn.entity.user.dtos;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserQueryDto {
+    private Long id;
+}
--- src/main/java/net/chatmindai/springboot3learn/mapper/UserMapper.java
+++ src/main/java/net/chatmindai/springboot3learn/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package net.chatmindai.springboot3learn.mapper;
 
 import net.chatmindai.springboot3learn.entity.user.User;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import net.chatmindai.springboot3learn.entity.user.dtos.UserQueryDto;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
@@ -15,6 +16,8 @@ import java.util.List;
 public interface UserMapper extends BaseMapper<User> {
 
     List<User> selectByname(@Param("name") String name);
+
+    User selectUserById(UserQueryDto dto);
 }
 
 
