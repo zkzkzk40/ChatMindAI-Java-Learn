77e703cc66ac40c6ce09a62e994d4c83a655dd09
吕成刚 <1330728137@qq.com>
2024-11-02 01:28:13 +0800

    AOP使用和Mybatis使用

--- src/main/java/com/example/Chatlearn/ChatlearnApplication.java
+++ src/main/java/com/example/Chatlearn/ChatlearnApplication.java
@@ -1,8 +1,9 @@
-package com.example.chatlearn;
+package com.example.Chatlearn;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan("com.example.Chatlearn.mapper")
 @SpringBootApplication
 public class ChatlearnApplication {
     public static void main(String[] args) {
--- src/test/java/com/example/Chatlearn/ChatlearnApplicationTests.java
+++ src/test/java/com/example/Chatlearn/ChatlearnApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.chatlearn;
+package com.example.Chatlearn;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
